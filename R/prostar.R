0068557a504225f236d212cd42d688ab467436c0
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2017-04-11 07:54:57 +0000

    Version 1.7.23
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Prostar@128521 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/prostar.R
+++ R/prostar.R
@@ -8,9 +8,10 @@
 Prostar <-
 function(){
         G <- .GlobalEnv
-
+if (interactive()){
     options(shiny.maxRequestSize=1024^3)
     a=shiny::runApp(system.file("ProstarApp",package="Prostar"),
                     launch.browser = TRUE)
     return(invisible(a))
+} else {return (NULL)}
 }
---
+++ tests/testthat.R
@@ -0,0 +1,8 @@
+Sys.setenv("R_TESTS" = "")
+
+library(testthat)
+library(shinytest)
+library(DAPAR)
+library(DAPARdata)
+
+test_check("Prostar")
