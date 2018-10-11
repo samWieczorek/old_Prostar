22f54863c62aca8aa185ad15908d2d554c6c614e
ThomasBurger <thomas.burger@cea.fr>
2018-10-11 10:52:24 +0200

    Update srv_Normalization.R
    MAJ des refs

--- inst/ProstarApp/server/srv_Normalization.R
+++ inst/ProstarApp/server/srv_Normalization.R
@@ -19,17 +19,27 @@ output$helpForNormalizationMethods <- renderUI({
     if (input$normalization.method == "None") {return(NULL)}
     
     switch(input$normalization.method,
-           GlobalQuantileAlignment= txt <- "These methods propose normalizations of important magnitude that should be cautiously used. It proposes to align the quantiles of all the replicates as described in [6]; practically it amounts to replace 
+           GlobalQuantileAlignment= txt <- "These methods propose normalizations of important
+           magnitude that should be cautiously used. It proposes to align the quantiles of all 
+           the replicates as described in [Other ref. 1]; practically it amounts to replace 
            abundances by order statistics.",
-           QuantileCentering = txt <- "These methods propose to shift the sample distributions (either all of them at once, or within each condition at a time) to align a specific quantile: the median (under the assumption that up-regulations and down-regulations are equally frequent), the 15% quantile (under the assumption that the signal/noise ratio is 
-    roughly the same in all the samples), or any other user's choice.",
-           MeanCentering = txt <- "These methods propose to shift the sample distributions (either all of them at once, or within each condition at a time) to align their means. It is also possible to force unit variance (or not).",
+           QuantileCentering = txt <- "These methods propose to shift the sample distributions 
+           (either all of them at once, or within each condition at a time) to align a specific 
+           quantile: the median (under the assumption that up-regulations and down-regulations 
+           are equally frequent), the 15% quantile (under the assumption that the signal/noise ratio is 
+           roughly the same in all the samples), or any other user's choice.",
+           MeanCentering = txt <- "These methods propose to shift the sample distributions (either all 
+           of them at once, or within each condition at a time) to align their means. It is also possible 
+           to force unit variance (or not).",
            SumByColumns = txt <- "These methods propose normalizations of important magnitude that should be cautiously used.
-    It operates on the original scale (not the log2 one) and propose to normalize each abundance by the total abundance of the sample (so as to focus on the analyte proportions among each sample).",
-           LOESS = txt <- "This method proposes to apply a cyclic LOESS [11, 12] normalization to the data (either all of them at once, or on each condition independently). It relates to  a 
+           It operates on the original scale (not the log2 one) and propose to normalize each abundance by the 
+           total abundance of the sample (so as to focus on the analyte proportions among each sample).",
+           LOESS = txt <- "This method proposes to apply a cyclic LOESS [Other ref. 4, 5] normalization to the data 
+           (either all of them at once, or on each condition independently). It relates to  a 
            combination of multiple regression models. The user can tune the regression span (an higher span smooths
            the fit more, while a lower span captures more trends).",
-           vsn = txt <- "This method proposes to apply the Variance Stabilization Normalization [13] to the data (either all of them at once, or on each condition independently). No specific parameters required."
+           vsn = txt <- "This method proposes to apply the Variance Stabilization Normalization [Other ref. 6] to the 
+           data (either all of them at once, or on each condition independently). No specific parameters required."
            )
    
     tags$p(txt)
