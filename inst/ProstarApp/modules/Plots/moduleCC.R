ff23491954310e5047b378a0e84eafced8dffc38
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2020-11-09 09:54:24 +0100

    bug fixed in compute CC

--- inst/ProstarApp/modules/Plots/moduleCC.R
+++ inst/ProstarApp/modules/Plots/moduleCC.R
@@ -584,7 +584,7 @@ output$CCDetailed <- renderUI({
   
   output$Warning_OneOneDTDetailed <- reactive({
     req(GetDataFor_OneOneDTDetailed())
-        browser()
+        #browser()
     if (nrow(GetDataFor_OneOneDTDetailed()) > 153) 
       p(MSG_WARNING_SIZE_DT)
     
