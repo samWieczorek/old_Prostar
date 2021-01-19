74c73ad2a32bcf2dbcd23de80b8a777706faa34d
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2021-01-19 17:49:51 +0100

    bug fixed with norm and track prot

--- inst/ProstarApp/server/srv_Normalization.R
+++ inst/ProstarApp/server/srv_Normalization.R
@@ -263,12 +263,27 @@ observeEvent(rv$widgets$normalization$method,{
 })
 
 
+etIndicesOfSelectedProteins_ForNorm <- reactive({
+  req(rv.norm$selectProt())
+  
+  ind <- NULL
+  ll <- fData(rv$current.obj)[,rv$current.obj@experimentData@other$proteinId]
+  tt <- rv.norm$selectProt()$type
+  switch(tt,
+         ProteinList = ind <- rv.norm$selectProt()$list.indices,
+         Random = ind <- rv.norm$selectProt()$rand.indices,
+         Column = ind <- rv.norm$selectProt()$col.indices
+  )
+  if (length(ind)==0)
+    ind <- NULL
+  
+
+  ind
+})
+
 GetIndicesOfSelectedProteins <- reactive({
   req(rv.norm$trackFromBoxplot())
   
-  
-  #print('in GetIndicesOfSelectedProteins')
-  #print(rv.norm$trackFromBoxplot())
   ind <- NULL
   ll <- fData(rv$current.obj)[,rv$current.obj@experimentData@other$proteinId]
   tt <- rv.norm$trackFromBoxplot()$type
@@ -279,9 +294,6 @@ GetIndicesOfSelectedProteins <- reactive({
   )
   if (length(ind)==0)
     ind <- NULL
-  
-  #print('ind = ')
-  #print(ind)
   ind
 })
 
@@ -308,7 +320,7 @@ observeEvent(input$perform.normalization,{
                                                 type = rv$widgets$normalization$type, 
                                                 cond = pData(rv$dataset[[input$datasets]])$Condition,
                                                 quantile = quant,
-                                                subset.norm = GetIndicesOfSelectedProteins())
+                                                subset.norm = GetIndicesOfSelectedProteins_ForNorm())
            
          } ,  
          MeanCentering = {
@@ -317,14 +329,14 @@ observeEvent(input$perform.normalization,{
                                                 conds = pData(rv$dataset[[input$datasets]])$Condition,
                                                 type = rv$widgets$normalization$type, 
                                                 scaling=  rv$widgets$normalization$varReduction,
-                                                subset.norm = GetIndicesOfSelectedProteins())
+                                                subset.norm = GetIndicesOfSelectedProteins_ForNorm())
          }, 
          SumByColumns = {
            rv$current.obj <- wrapper.normalizeD(obj = rv$dataset[[input$datasets]], 
                                                 method = rv$widgets$normalization$method, 
                                                 conds = pData(rv$dataset[[input$datasets]])$Condition,
                                                 type = rv$widgets$normalization$type,
-                                                subset.norm = GetIndicesOfSelectedProteins())
+                                                subset.norm = GetIndicesOfSelectedProteins_ForNorm())
            
          },
          LOESS = { rv$current.obj <- wrapper.normalizeD(obj = rv$dataset[[input$datasets]], 
