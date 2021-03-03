1835ca96887c1e083e6c0fb931abafb791ca5dab
FREMY Enora <enora.fremy@orange.fr>
2021-03-03 11:49:34 +0100

    filtration example ok, but still an switch error

--- inst/ProstarApp/dev/moduleFiltrationExamples.R
+++ inst/ProstarApp/dev/moduleFiltrationExamples.R
@@ -1,5 +1,3 @@
-# setwd('~/TELETRAVAIL/github_master/Prostar/inst/ProstarApp/')
-
 library(shiny)
 library(DT)
 library(DAPAR)
@@ -14,16 +12,14 @@ gFiltersList <- c("None" = "None",
 gFilterEmptyLines <- gFiltersList[["Empty lines"]]
 
 
-# create the MSnset dataset to use DAPAR function getListNbValuesInLines
 # table example
 plop <- read.csv('dev/example_filtration_tab.txt', sep='\t')
-
+# create the MSnset dataset to use DAPAR function getListNbValuesInLines
 metadata_plop <- as.data.frame(matrix(NA, nrow=6, ncol=3))
 colnames(metadata_plop) <- c("Sample.name","Condition","Bio.Rep")
 metadata_plop$Sample.name <- colnames(plop)
 metadata_plop$Condition <- c(rep("c1",3),rep("c2",3))
 metadata_plop$Bio.Rep <- c(1:6)
-
 plop_msnset <- DAPAR::createMSnset(file = 'dev/example_filtration_tab.txt', indExpData = c(1:6), metadata = metadata_plop)
 
 
@@ -31,14 +27,15 @@ plop_msnset <- DAPAR::createMSnset(file = 'dev/example_filtration_tab.txt', indE
 
 ui <- fluidPage(
   
-  dataTableOutput("buildFiltrationExample"),
+  fluidRow(column(4,selectInput("ChooseFilters","",
+                                choices = gFiltersList,
+                                selected = "None",
+                                width='200px')),
+           column(8,textOutput("methodInformation"))),
   
-  selectInput("ChooseFilters","",
-                  choices = gFiltersList,
-                  width='200px'),
+  uiOutput("seuilNADelete"),
   
-  uiOutput("seuilNADelete")
-  
+  dataTableOutput("buildFiltrationExample")
   
 )
 
@@ -46,17 +43,15 @@ ui <- fluidPage(
 server <- function(input, output, session){
   
   
-  
   output$seuilNADelete <- renderUI({
     req(input$ChooseFilters)
     
     if ((input$ChooseFilters=="None") || (input$ChooseFilters==gFilterEmptyLines)) {
-      return(NULL)   
+      return(NULL)
     }
     
     tagList(
       shinyjs::useShinyjs(),
-      
       uiOutput('keepVal_ui')
     )
   })
@@ -68,31 +63,44 @@ server <- function(input, output, session){
     
     tagList(
       selectInput("seuilNA", NULL,
-                  choices =  DAPAR::getListNbValuesInLines(plop_msnset, 
+                  choices =  DAPAR::getListNbValuesInLines(plop_msnset,
                                                            type=input$ChooseFilters),
+                  selected = "0",
                   width='150px')
     )
   })
   
   
+  output$methodInformation <- renderText({
+    switch(input$ChooseFilters,
+           None = {
+             txt <- "No lines are removed"
+           },
+           EmptyLines = {
+             txt <- "Lines with only NAs are removed."
+           },
+           WholeMatrix = { 
+             txt <- paste("Lines containing at least",
+                          input$seuilNA,
+                          "quantitative values per row are kept.")
+           },
+           AllCond = {
+             txt <- paste("Lines containing at least",
+                          input$seuilNA,
+                          "quantitative values per row, in all condition, are kept.")
+           },
+           AtLeastOneCond = {
+             txt <- paste("Lines containing at least",
+                          input$seuilNA,
+                          "quantitative values per row, in at least one condition, are kept.")
+           }
+    )
+    txt
+  })
+  
   output$buildFiltrationExample <- DT::renderDataTable({
-    
-    
-    index<-reactiveValues(l<-NULL)
-    # # table example
-    # content <- c(1,rep(NA,10),
-    #              rep(1,2),rep(NA,5),rep(1,3),NA,
-    #              rep(1,3),rep(NA,4),rep(1,4),
-    #              rep(1,4),rep(NA,7),
-    #              rep(1,5),rep(NA,2),1,rep(NA,3),
-    #              rep(1,6),NA,rep(1,2),NA,1
-    # )
-    # df <- as.data.frame(matrix(content, nrow=11, ncol=6))
-    # colnames(df) <- c("A1","A2","A3","B1","B2","B3")
-    # rownames(df) <- c(1:11)
-    
-    
-    #    A1 A2 A3 B1 B2 B3
+    ################################################################
+    # A1 A2 A3 B1 B2 B3
     # 1   1  1  1  1  1  1
     # 2  NA  1  1  1  1  1
     # 3  NA NA  1  1  1  1
@@ -107,90 +115,70 @@ server <- function(input, output, session){
     
     # Indices for filtered rows, to darken:
     # paramtype<-c("None", "EmptyLines", "WholeMatrix", "AllCond", "AtLeastOneCond")
-    # "l_none", nothing to darken, keep all row
-    # l_emptyLines <- 7
-    # l_wholeMatrix <- list("th1"=c(7),"th2"=c(6,7),"th3"=c(5:7,10,11),"th4"=c(4:7,9,10,11),"th5"=c(3:12),"th6"=c(2:11))
-    # l_allCond <-list("th1"=c(4:7,10),"th2"=c(3:7,9,10,11),"th3"=c(2:11))
-    # l_atLeastOneCondition <- list("th1"=c(7),"th2"=c(6,7,11),"th3"=c(5:11))
+    # none: nothing to darken, keep all row
+    # emptyLines: 7
+    # wholeMatrix: list("th1"=c(7),"th2"=c(6,7),"th3"=c(5:7,10,11),"th4"=c(4:7,9,10,11),"th5"=c(3:11),"th6"=c(2:11))
+    # allCond: list("th1"=c(4:7,10),"th2"=c(3:7,9,10,11),"th3"=c(2:11))
+    # atLeastOneCondition: list("th1"=c(7),"th2"=c(6,7,11),"th3"=c(5:11))
     
     # Example with method=WholeMatrix/th=3 <=> at least 3 quanti values by entire row
     # index <- c(5,6,7,10,11)
-    #################################################################
+    ################################################################
     switch(input$ChooseFilters,
-           emptyLines= {
-             index <- reactiveValues(l<-7)
+           None = {
+             index <- NULL
            },
-           WholeMatrix= {
-             index <- reactiveValues(l<-c(5,6,7,10,11))
+           EmptyLines = {
+             index <- 7
            },
+           WholeMatrix = { switch(input$seuilNA,
+                                  "0" = { index <- NULL },
+                                  "1" = { index <- 7 },
+                                  "2" = { index <- c(6,7) },
+                                  "3" = { index <- c(5:7,10,11) },
+                                  "4" = { index <- c(4:7,9,10,11) },
+                                  "5" = { index <- c(3:11)},
+                                  "6" = { index <- c(2:11)}
+           )},
            AllCond = { switch(input$seuilNA,
-                              # 0 = {
-                              #   index <- 
-                              # },
-                              "1" = {
-                                index <- reactiveValues(l<-c(4:7,10))
-                              },
-                              "2" = {
-                                index <- reactiveValues(l<-c(3:7,9,10,11))
-                              },
-                              "3" = {
-                                index <- reactiveValues(l<-c(2:11))
-                              },
+                              "0" = { index <- NULL },
+                              "1" = { index <- c(4:7,10) },
+                              "2" = { index <- c(3:7,9,10,11) },
+                              "3" = { index <- c(2:11) }
            )},
            AtLeastOneCond = { switch(input$seuilNA,
-                                     # 0 = {
-                                     #   index <- 
-                                     # },
-                                     "1" = {
-                                       index <- reactiveValues(l<-7)
-                                     },
-                                     "2" = {
-                                       index <- reactiveValues(l<-c(6,7,11))
-                                     },
-                                     "3" = {
-                                       index <- reactiveValues(l<-c(5:11))
-                                     },
+                                     "0" = { index <- NULL },
+                                     "1" = { index <- 7 },
+                                     "2" = { index <- c(6,7,11) },
+                                     "3" = { index <- c(5:11) }
            )}
     )
-    print("input$ChooseFilters")
-    print(input$ChooseFilters)
-    print("input$seuilNA")
-    print(input$seuilNA)
-    print("index")
-    print(index)
-    #################################################################
-    if (!is.null(index()$l)){
-      print("ici")
-      dt <- datatable(exprs(plop_msnset),
-                      options = list(
-                        paging = FALSE,
-                        searching = FALSE)) %>%
+    
+    dt <- plop
+    
+    if (!is.null(index)){
+      datatable(dt,
+                options = list(
+                  paging = FALSE,
+                  searching = FALSE)) %>%
         formatStyle(
           .,
           columns = 1,
           valueColumns = 0,
           target = 'row',
-          backgroundColor = styleEqual(index()$l, rep('grey', length(index()$l))) 
+          backgroundColor = styleEqual(index, rep('grey', length(index)) )
         )
-      
-      dt
     } else {
-      print("la")
-      dt <- datatable(exprs(plop_msnset),
-                      options = list(
-                        paging = FALSE,
-                        searching = FALSE))
-      
-      dt
+      datatable(dt,
+                options = list(
+                  paging = FALSE,
+                  searching = FALSE))
     }
-
-    
-    
-    
-    
   })
+  
 }
 
 
-
 shinyApp(ui, server)
+
+
